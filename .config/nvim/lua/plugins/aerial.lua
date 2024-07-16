95b7fc85a988aad5fa5529aa2bd06503f3f6f07e
ejrichards <1760589+ejrichards@users.noreply.github.com>
2024-07-16 10:48:30 -0400

    Updating aerial config

--- .config/nvim/lua/plugins/aerial.lua
+++ .config/nvim/lua/plugins/aerial.lua
@@ -1,18 +1,27 @@
 return {
 	"stevearc/aerial.nvim",
+	version = "*",
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter",
 		"nvim-tree/nvim-web-devicons"
 	},
-	config = function()
-		require("aerial").setup({
-			layout = {
-				default_direction = "float"
-			},
-			close_automatic_events = { "unfocus" }
-		})
-
-		vim.keymap.set("n", "go", "<Cmd>AerialNavOpen<CR>")
-		vim.keymap.set("n", "gO", "<Cmd>AerialOpen<CR>")
-	end
+	opts = {
+		layout = {
+			default_direction = "float"
+		},
+		close_automatic_events = { "unfocus" },
+		on_attach = function(bufnr)
+			vim.keymap.set("n", "go", "<Cmd>AerialNavOpen<CR>", { buffer = bufnr })
+			vim.keymap.set("n", "gO", "<Cmd>AerialOpen<CR>", { buffer = bufnr })
+		end,
+		keymaps = {
+			["<Esc>"] = "actions.close",
+		},
+		nav = {
+			keymaps = {
+				["<Esc>"] = "actions.close",
+				["q"] = "actions.close",
+			}
+		},
+	}
 }
