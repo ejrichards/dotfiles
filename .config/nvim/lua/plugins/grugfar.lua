f374eaa13edb3f22d49dbd5bb4a2e47f9c258e40
ejrichards <1760589+ejrichards@users.noreply.github.com>
2024-07-22 17:25:02 -0400

    Adding grug-far

---
+++ .config/nvim/lua/plugins/grugfar.lua
@@ -0,0 +1,18 @@
+return {
+	'MagicDuck/grug-far.nvim',
+	config = function()
+		local far = require('grug-far')
+		far.setup({
+			windowCreationCommand = 'tabnew %',
+		})
+
+		vim.keymap.set({ 'n', 'x' }, '<leader>er', far.grug_far, { desc = 'grug-far' })
+
+		local far_augroup = vim.api.nvim_create_augroup('grug-far', { clear = true })
+		vim.api.nvim_create_autocmd('FileType', {
+			pattern = { 'grug-far' },
+			group = far_augroup,
+			command = 'setl bufhidden=wipe'
+		})
+	end,
+}
