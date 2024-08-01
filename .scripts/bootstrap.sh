48827b50798a568a3a2cf8d5c62375638548819e
ejrichards <1760589+ejrichards@users.noreply.github.com>
2024-08-01 17:49:13 -0400

    Adding scripts folder

---
+++ .scripts/bootstrap.sh
@@ -0,0 +1,14 @@
+#/usr/bin/env bash
+set -x
+
+alias dot="git --git-dir=$HOME/.dotgit --work-tree=$HOME"
+
+cd
+git clone --bare https://github.com/ejrichards/dotfiles.git .dotgit
+dot config --local status.showUntrackedFiles no
+dot config --local core.logAllRefUpdates true
+dot config --local remote.origin.fetch '+refs/heads/*:refs/remotes/origin/*'
+dot config --local branch.master.remote origin
+dot config --local branch.master.merge refs/heads/master
+dot fetch
+dot checkout
