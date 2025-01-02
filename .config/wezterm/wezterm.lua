b907c74d8912c051677d445aa1ce98c82296541e
ejrichards <1760589+ejrichards@users.noreply.github.com>
2025-01-01 23:24:42 -0500

    Terminal tweaks

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -8,6 +8,41 @@ config.font_size = 13.0
 config.use_fancy_tab_bar = false
 config.tab_max_width = 26
 
+local MINIMIZE = wezterm.nerdfonts.cod_chrome_minimize
+local MAXIMIZE = wezterm.nerdfonts.cod_chrome_maximize
+local CLOSE = wezterm.nerdfonts.cod_chrome_close
+local HOVER_COLOR = { Color = '#aaa' }
+config.tab_bar_style = {
+	new_tab = wezterm.format {
+		{ Text = ' + ' },
+	},
+	new_tab_hover = wezterm.format {
+		{ Foreground = HOVER_COLOR },
+		{ Text = ' + ' },
+	},
+	window_hide = wezterm.format {
+		{ Text = ' ' .. MINIMIZE .. ' ' },
+	},
+	window_hide_hover = wezterm.format {
+		{ Foreground = HOVER_COLOR },
+		{ Text = ' ' .. MINIMIZE .. ' ' },
+	},
+	window_maximize = wezterm.format {
+		{ Text = ' ' .. MAXIMIZE .. ' ' },
+	},
+	window_maximize_hover = wezterm.format {
+		{ Foreground = HOVER_COLOR },
+		{ Text = ' ' .. MAXIMIZE .. ' ' },
+	},
+	window_close = wezterm.format {
+		{ Text = ' ' .. CLOSE .. ' ' },
+	},
+	window_close_hover = wezterm.format {
+		{ Foreground = HOVER_COLOR },
+		{ Text = ' ' .. CLOSE .. ' ' },
+	},
+}
+
 config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
 config.window_padding = {
 	left = '0.5cell',
@@ -38,6 +73,8 @@ config.colors = {
 	}
 }
 
+config.enable_kitty_keyboard = true
+
 local keys = {
 	{ key = 'Tab', mods = 'SHIFT|CTRL', action = wezterm.action.ShowLauncherArgs({ flags = 'LAUNCH_MENU_ITEMS|TABS' }) },
 	-- Swap these two
@@ -46,6 +83,8 @@ local keys = {
 }
 
 if wezterm.target_triple:find('windows') then
+	config.allow_win32_input_mode = false
+
 	for _, gpu in ipairs(wezterm.gui.enumerate_gpus()) do
 		if gpu.backend == 'Dx12' and gpu.device_type == 'DiscreteGpu' then
 			config.webgpu_preferred_adapter = gpu
@@ -56,19 +95,23 @@ if wezterm.target_triple:find('windows') then
 	config.default_prog = { 'elvish.exe' }
 
 	local elvish = { label = 'Elvish', args = { 'elvish.exe' } }
-	local powershell = { label = 'PowerShell', args = { 'pwsh.exe', '-NoLogo' } }
 	local cmd = { label = 'cmd', args = { 'cmd.exe' } }
-	local ubuntu = { label = 'Ubuntu', args = { 'ubuntu.exe' } }
+	local ubuntu = { label = 'Ubuntu', args = { 'wsl.exe', '-d', 'Ubuntu' } }
+	local nixos = { label = 'NixOS', args = { 'wsl.exe', '-d', 'NixOS' } }
+	local powershell = { label = 'PowerShell', args = { 'pwsh.exe', '-NoLogo' } }
 
 	config.launch_menu = {
-		powershell,
+		elvish,
 		cmd,
-		ubuntu
+		ubuntu,
+		nixos,
+		powershell,
 	}
 	table.insert(keys, { key = 'phys:1', mods = 'SHIFT|CTRL', action = wezterm.action.SpawnCommandInNewTab(elvish) })
 	table.insert(keys, { key = 'phys:2', mods = 'SHIFT|CTRL', action = wezterm.action.SpawnCommandInNewTab(cmd) })
 	table.insert(keys, { key = 'phys:3', mods = 'SHIFT|CTRL', action = wezterm.action.SpawnCommandInNewTab(ubuntu) })
-	table.insert(keys, { key = 'phys:4', mods = 'SHIFT|CTRL', action = wezterm.action.SpawnCommandInNewTab(powershell) })
+	table.insert(keys, { key = 'phys:4', mods = 'SHIFT|CTRL', action = wezterm.action.SpawnCommandInNewTab(nixos) })
+	table.insert(keys, { key = 'phys:5', mods = 'SHIFT|CTRL', action = wezterm.action.SpawnCommandInNewTab(powershell) })
 else
 	-- Not working in Windows WebGpu right now
 	config.window_background_opacity = 0.95
